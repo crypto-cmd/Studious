802860977b120c407f4ca6699a13e47dd023b0e0
MilleniasOp <migueldavyjr3@gmail.com>
2026-04-04 21:18:08 -0400

    The grade predictor computation

---
+++ backend/computations/GradePredictor.py
@@ -0,0 +1,46 @@
+
+
+class GradePredictor:
+
+    def __init__(self, predicted_grade):
+        self.predicted_grade = predicted_grade
+    
+    def calculate_slope(self, predicted_grade):
+        n = len(predicted_grade)
+        #predicted_grade = [(1, 70), (2, 75), (3, 80), (4, 85), (5, 90)]
+        months, grades = zip(*predicted_grade)
+
+        sum_x = sum(months)
+        sum_y = sum(grades)
+        sum_xy = sum(x*y for x, y in zip(months, grades))
+        sum_x_squared = sum(x**2 for x in months)
+
+        slope = (n * sum_xy - sum_x * sum_y) / (n * sum_x_squared - sum_x ** 2)
+        return slope
+
+    def calculate_intercept(self, predicted_grade, slope):
+        n = len(predicted_grade)
+        sum_x = sum(x for x, y in predicted_grade)
+        sum_y = sum(y for x, y in predicted_grade)
+
+        intercept = (sum_y - slope * sum_x) / n
+        return intercept
+    
+    def predict_final_grade(self, final_months):
+        slope = self.calculate_slope(self.predicted_grade)
+        intercept = self.calculate_intercept(self.predicted_grade, slope)
+
+        predicted_final_grade = slope * final_months + intercept
+        return predicted_final_grade
+
+      
+# ================= MAIN =================
+if __name__ == "__main__":
+    predicted_grade = [(1, 70), (2, 75), (3, 80), (4, 85), (5, 90)]
+
+    grade_predictor = GradePredictor(predicted_grade)
+
+    final_months = 6
+    predicted_final_grade = grade_predictor.predict_final_grade(final_months)
+
+    print(f"Predicted final grade after {final_months} months: {predicted_final_grade:.2f}")
